cf7aa01cf4aceb999d2ce050f6e7c3e9037e8fff
bandura <banduraweb@ukr.net>
2020-03-31 14:19:25 +0300

    Updates

---
+++ precache-manifest.1287dfe6a20513e86ad5f23d00bb57f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71f4cf6730a2044652096b77964c394",
+    "url": "/test-constructor/index.html"
+  },
+  {
+    "revision": "48d19c2cd087e1881f2f",
+    "url": "/test-constructor/static/css/main.74ddbdf0.chunk.css"
+  },
+  {
+    "revision": "82097415304b0d2a340e",
+    "url": "/test-constructor/static/js/2.37ee8cfc.chunk.js"
+  },
+  {
+    "revision": "a22aa98d10ace1f503f99f3f377d1cdc",
+    "url": "/test-constructor/static/js/2.37ee8cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d19c2cd087e1881f2f",
+    "url": "/test-constructor/static/js/main.deac4ccc.chunk.js"
+  },
+  {
+    "revision": "afe56741a35a487af3a1",
+    "url": "/test-constructor/static/js/runtime-main.dbadf840.js"
+  },
+  {
+    "revision": "259743a604b12093262075b2286c6485",
+    "url": "/test-constructor/static/media/tile.259743a6.png"
+  }
+]);
